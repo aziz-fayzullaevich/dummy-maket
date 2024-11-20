c05a305a6c22c2b339778a2b11058b94c65bd0e2
Aziz Atagullaev <azizatagullaev1@gmail.com>
2024-11-20 08:46:35 +0500

    dummy-maket

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// AOS
+AOS.init();
+
+// swiper
+var swiper = new Swiper(".mySwiper", {
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+var swiper = new Swiper(".swiper-gallery", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    freeMode: true,
+    autoplay: {
+        delay: 1500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
